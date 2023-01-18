5c725703e29d651b66754a92b3be2f5f11be13cb
FierySasquatch168 <9120603@gmail.com>
2023-01-18 17:55:13 +0200

    Functions for accessing the model created in ReminderListViewController+DataSource. Button configuration and action ready. Button logic settled, button action applies for the reminder with reminderID. Snapshot update for UI update implemented. Accessibility values added.

---
+++ Today/ReminderDoneButton.swift
@@ -0,0 +1,13 @@
+//
+//  ReminderDoneButton.swift
+//  Today
+//
+//  Created by Aleksandr Eliseev on 18.01.2023.
+//
+
+import UIKit
+
+class ReminderDoneButton: UIButton {
+    var id: Reminder.ID?
+    
+}
