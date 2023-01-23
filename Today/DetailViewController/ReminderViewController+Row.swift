f0f9eef5075ba1eb06b0c4ff017275d2a4517b95
FierySasquatch168 <9120603@gmail.com>
2023-01-23 12:57:33 +0200

    Editing mode added. View mode and editing mode apply different snapshots for showing content.

--- Today/DetailViewController/ReminderViewController+Row.swift
+++ Today/DetailViewController/ReminderViewController+Row.swift
@@ -14,6 +14,7 @@ extension ReminderViewController {
         case viewNotes
         case viewTime
         case viewTitle
+        case header(String)
         
         var imageName: String? {
             switch self {
